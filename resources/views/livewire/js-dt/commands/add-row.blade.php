b08a8746f8b59e814f72cf7952fddc367f175e88
Andreas Klebe <a.klebe.work@gmail.com>
2024-12-01 16:16:42 +0100

    - Preparing Frontend Imports

--- resources/views/livewire/js-dt/commands/add-row.blade.php
+++
@@ -1,8 +0,0 @@
-@php
-    /** @var \Modules\DataTable\app\Http\Livewire\DataTable\Base\BaseDataTable $this */
-    /** @var string $collectionName */
-@endphp
-<button
-        class="btn btn-success"
-        wire:click="$dispatchTo('{{ $this->relatedLivewireForm }}', 'open-form', {id: 0})"
-><span class="bi bi-plus-square"></span></button>
