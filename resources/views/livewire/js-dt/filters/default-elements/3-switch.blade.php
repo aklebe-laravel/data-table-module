3b02a3b466a6c8de1a5bc39bc08bca9bc48c9b2c
Andreas Klebe <a.klebe.work@gmail.com>
2025-01-29 15:41:24 +0100

    - fixed and reviewed offer blades - form frontendKey to const - added offer filters - fixed offer mismatch id/shared_id

--- resources/views/livewire/js-dt/filters/default-elements/3-switch.blade.php
+++ resources/views/livewire/js-dt/filters/default-elements/3-switch.blade.php
@@ -9,16 +9,22 @@
     $systemService = app('system_base');
     $htmlIdSuffix = $systemService->addUniqueCounter('switch3-id');
     $filterName = "filters.{$collectionName}.{$_config['name']}";
+    $value = data_get($this, $filterName);
+    $defaultClass = $this->isFilterDefault($collectionName, $_config['name']) ? 'bg-light-subtle' : '';
 @endphp
 <div class="switch3">
-    <input wire:model.live.debounce="{{ $filterName }}" type="radio" id="switch3-radio1-{{ $htmlIdSuffix }}" value="{{ NativeObjectBase::switch3No }}"/>
-    <label for="switch3-radio1-{{ $htmlIdSuffix }}"><span class="bi bi bi-hand-thumbs-down"></span></label>
+    <input wire:model.live.debounce="{{ $filterName }}" type="radio" id="switch3-radio1-{{ $htmlIdSuffix }}"
+           value="{{ NativeObjectBase::switch3No }}"
+    />
+    <label for="switch3-radio1-{{ $htmlIdSuffix }}" class="{{ ($value == NativeObjectBase::switch3No) ? $defaultClass : '' }}"><span class="bi bi bi-hand-thumbs-down"></span></label>
 
     <input wire:model.live.debounce="{{ $filterName }}" type="radio" id="switch3-radio2-{{ $htmlIdSuffix }}"
-           class="unused"
-           value="{{ NativeObjectBase::switch3Unused }}"/>
-    <label for="switch3-radio2-{{ $htmlIdSuffix }}" class="{{ $this->isFilterDefault($collectionName, $_config['name']) ? 'bg-light-subtle' : '' }}">{{ $_config['label'] }}</label>
+           value="{{ NativeObjectBase::switch3Unused }}"
+    />
+    <label for="switch3-radio2-{{ $htmlIdSuffix }}" class="{{ ($value == NativeObjectBase::switch3Unused) ? $defaultClass : '' }}">{{ $_config['label'] }}</label>
 
-    <input wire:model.live.debounce="{{ $filterName }}" type="radio" id="switch3-radio3-{{ $htmlIdSuffix }}" value="{{ NativeObjectBase::switch3Yes }}"/>
-    <label for="switch3-radio3-{{ $htmlIdSuffix }}"><span class="bi bi bi-hand-thumbs-up"></span></label>
+    <input wire:model.live.debounce="{{ $filterName }}" type="radio" id="switch3-radio3-{{ $htmlIdSuffix }}"
+           value="{{ NativeObjectBase::switch3Yes }}"
+    />
+    <label for="switch3-radio3-{{ $htmlIdSuffix }}" class="{{ ($value == NativeObjectBase::switch3Yes) ? $defaultClass : '' }}"><span class="bi bi bi-hand-thumbs-up"></span></label>
 </div>
